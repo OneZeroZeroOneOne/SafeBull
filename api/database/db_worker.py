3f6f113398ea4ab5ee20b1638c0e39843b617a08
OneZeroZeroOneOne <intheendreallife@gmail.com>
2021-06-21 16:41:30 +0300

    a

--- api/database/db_worker.py
+++ api/database/db_worker.py
@@ -5,7 +5,7 @@ class DBWorker:
         self.conn = conn
     
     async def get_all_tokens_output(self):
-        sql = """select tot."id", u."id" as user_id, tot.tokens ,tot.created_date_time from "tokens_output" tot join "user" u on tot.user_id = u."id""""
+        sql = """select tot."id", u."id" as user_id, tot.tokens ,tot.created_date_time from "tokens_output" tot join "user" u on tot.user_id = u."id" """
         return await self.conn.fetch(sql)
     
     async def delete_tokens_output(self, id):
